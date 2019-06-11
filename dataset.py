67896f3004ae4c89cd2c1f65b92d6b39d720b4f2
vosar <ivankosarevych007@gmail.com>
2019-06-11 15:50:15 +0300

    dataset init started

--- dataset.py
+++ dataset.py
@@ -1,9 +1,17 @@
 from torch.utils.data import Dataset
 
 
-class LJSpeechDataset(Dataset):
-    def __init__(self):
-        pass
+class TextSpeechDataset(Dataset):
+    def __init__(self, root_dir, parameters):
+        """
+        :param root_dir: path to data
+        :param parameters: dict with parameters
+        """
+        self.root_dir = root_dir
+        self.cleaners = parameters["text_cleaner"]
+        self.outputs_per_step = parameters["outputs_per_step"]
+        self.sample_rate = parameters["sample_rate"]
+        self.min_seq_len = parameters["min_seq_len"]
 
     def __len__(self):
         pass
