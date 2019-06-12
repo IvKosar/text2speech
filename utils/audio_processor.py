363fa06fa77b575b5603ed595314ea4fdc98b6a3
vosar <ivankosarevych007@gmail.com>
2019-06-12 19:15:21 +0300

    audio utils

--- utils/audio_processor.py
+++ utils/audio_processor.py
@@ -1,10 +1,13 @@
+import numpy as np
 from scipy import signal
 from librosa import stft, filters
-from utils.audio_utils import *
+from utils.audio_utils import amplitude_to_dbs, dbs_to_amplitude, normalize_spectrogram, denormalize_spectrogram,\
+    griffin_lim
 
 
 class AudioProcessor:
-    def __init__(self, sample_rate, preemphasis, frequency, frame_length, frame_shift, min_dbs, ref_dbs, mels_size):
+    def __init__(self, sample_rate, preemphasis, frequency, frame_length, frame_shift, min_dbs, ref_dbs, mels_size,
+                 griff_lim_iters, power):
         self.sample_rate = sample_rate
         self.preemphasis = preemphasis
         self.frequency = frequency
@@ -13,6 +16,8 @@ class AudioProcessor:
         self.min_dbs = min_dbs
         self.ref_dbs = ref_dbs
         self.mels_size = mels_size
+        self.griff_lim_iters = griff_lim_iters
+        self.power = power
 
         self.n_fft = (self.frequency - 1) * 2
         self.hop_length = int(frame_shift / 1000 * sample_rate)
@@ -20,19 +25,23 @@ class AudioProcessor:
 
         self.mel_basis = filters.mel(self.sample_rate, self.n_fft, n_mels=self.mels_size)
 
-    def generate_spectrogram(self, waveform):
+    def wav_to_linear_spectrogram(self, waveform):
         filtered_wav = signal.lfilter([1, -self.preemphasis], [1], waveform)
         linear_spectrogram = stft(y=filtered_wav, n_fft=self.n_fft, hop_length=self.hop_length, win_length=self.win_length)
         spectrogram_dbs = amplitude_to_dbs(np.abs(linear_spectrogram)) - self.ref_dbs
         return normalize_spectrogram(spectrogram_dbs, self.min_dbs)
 
-    def generate_mel_spectrogram(self, waveform):
+    def wav_to_mel_spectrogram(self, waveform):
         filtered_wav = signal.lfilter([1, -self.preemphasis], [1], waveform)
         linear_spectrogram = stft(y=filtered_wav, n_fft=self.n_fft, hop_length=self.hop_length, win_length=self.win_length)
         mel_spectorgram = np.dot(self.mel_basis, np.abs(linear_spectrogram))
         mel_dbs = amplitude_to_dbs(mel_spectorgram) - self.ref_dbs
         return normalize_spectrogram(mel_dbs, self.min_dbs)
 
-
+    def spectrogram_to_wav(self, spectrogram):
+        denormalized = denormalize_spectrogram(spectrogram, self.min_dbs)
+        linear = dbs_to_amplitude(denormalized + self.ref_dbs)
+        waveform = griffin_lim(linear ** self.power, self.griff_lim_iters, self.n_fft, self.hop_length, self.win_length)
+        return signal.lfilter([1], [1 - self.preemphasis], waveform)
 
 
--- utils/audio_utils.py
+++ utils/audio_utils.py
@@ -1,4 +1,15 @@
 import numpy as np
+import librosa
+
+
+def save_wav(wav, path, sample_rate):
+    wav *= 32767 / max(0.01, np.max(np.abs(wav)))
+    librosa.output.write_wav(path, wav.astype(np.float), sample_rate)
+
+
+def load_wav(filename, sample_rate):
+    audio = librosa.core.load(filename, sr=sample_rate)
+    return audio
 
 
 def amplitude_to_dbs(amplitude):
@@ -9,9 +20,32 @@ def dbs_to_amplitude(dbs):
     return np.power(10.0, dbs * 0.05)
 
 
-def normalize_spectrogram(dbs, min_level_db):
-    return np.clip((dbs - min_level_db) / -min_level_db, 0, 1)
+def normalize_spectrogram(dbs, min_dbs):
+    return np.clip((dbs - min_dbs) / -min_dbs, 0, 1)
 
 
-def denormalize_spectrogram(dbs, min_level_db):
-    return (np.clip(dbs, 0, 1) * -min_level_db) + min_level_db
+def denormalize_spectrogram(dbs, min_dbs):
+    return (np.clip(dbs, 0, 1) * -min_dbs) + min_dbs
+
+
+def griffin_lim(spectrogram, griffin_lim_iters, n_fft, hop_length, win_length):
+    '''librosa implementation of Griffin-Lim
+    Based on https://github.com/librosa/librosa/issues/434
+    '''
+    angles = np.exp(2j * np.pi * np.random.rand(*spectrogram.shape))
+    spectrogram_complex = np.abs(spectrogram).astype(np.complex)
+    y = librosa.istft(spectrogram_complex * angles, hop_length=hop_length, win_length=win_length)
+    for i in range(griffin_lim_iters):
+        angles = np.exp(1j * np.angle(librosa.stft(y=y, n_fft=n_fft, hop_length=hop_length, win_length=win_length)))
+        y = librosa.istft(spectrogram_complex * angles, hop_length=hop_length, win_length=win_length)
+    return y
+
+
+def find_endpoint(wav, sample_rate, threshold_db=-40, min_silence_sec=0.8):
+    window_length = int(sample_rate * min_silence_sec)
+    hop_length = int(window_length / 4)
+    threshold = dbs_to_amplitude(threshold_db)
+    for x in range(hop_length, len(wav) - window_length, hop_length):
+        if np.max(wav[x:x + window_length]) < threshold:
+            return x + hop_length
+    return len(wav)
