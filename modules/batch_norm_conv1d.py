19b4fdddc7ef5efd7754fb95162be566c4227e41
Marian Petruk <marian.petruk@gmail.com>
2019-06-13 16:11:50 +0300

    fix batchnorm

--- modules/batch_norm_conv1d.py
+++ modules/batch_norm_conv1d.py
@@ -6,7 +6,7 @@ class BatchNormConv1d(nn.Module):
         super(BatchNormConv1d, self).__init__()
         self.conv1d = nn.Conv1d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size,
                                 stride=stride, padding=padding, bias=False)
-        self.batch_norm = nn.BatchNorm1d(out_channels=out_channels, momentum=0.99, eps=1e-3)
+        self.batch_norm = nn.BatchNorm1d(num_features=out_channels, momentum=0.99, eps=1e-3)
         self.activation = activation
 
     def forward(self, x):
