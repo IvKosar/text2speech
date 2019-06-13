c2fa778a4312980414041be7cbdb3a613d8ce24c
Marian Petruk <marian.petruk@gmail.com>
2019-06-13 13:48:31 +0300

    optimize networks

--- networks/decoder.py
+++ networks/decoder.py
@@ -1,24 +1,26 @@
-from torch.nn import Module, ModuleList, GRUCell, Linear
 from torch import stack, cat
 from torch.autograd import Variable
-from modules.prenet import Prenet
+from torch.nn import Module, ModuleList, GRUCell, Linear
+
 from modules.attention import AttentionRNN
+from modules.prenet import Prenet
 
 
 class Decoder(Module):
-    def __init__(self, in_features, memory_dim, r, eps=0, mode='train'):
+    def __init__(self, in_features, memory_dim, r, eps=0):
         super(Decoder, self).__init__()
-        self.mode = mode
         self.max_decoder_steps = 200
         self.memory_dim = memory_dim
         self.eps = eps
         self.r = r
 
-        self.prenet = Prenet(in_features=(memory_dim * r), out_features=[256, 128])
+        self.prenet = Prenet(in_features=(self.memory_dim * self.r), out_features=[256, 128])
         self.attention_rnn = AttentionRNN(out_dim=256, annot_dim=in_features, memory_dim=128)
-        self.project_to_decoder_in = Linear(in_features=(256+in_features), out_features=256)
-        self.decoder_rnns = ModuleList([GRUCell(256, 256), GRUCell(256, 256)])  # 2-layer residual GRU (256 cells)
-        self.proj_to_mel = Linear(256, memory_dim * r)
+        self.project_to_decoder_in = Linear(in_features=(256 + in_features), out_features=256)
+        # 2-layer residual GRU (256 cells)
+        self.decoder_rnns = ModuleList(
+            [GRUCell(input_size=256, hidden_size=256), GRUCell(input_size=256, hidden_size=256)])
+        self.proj_to_mel = Linear(in_features=256, out_features=(self.memory_dim * self.r))
 
     def forward(self, inputs, memory=None):
         B = inputs.size(0)
@@ -28,13 +30,11 @@ class Decoder(Module):
                 memory = memory.view(B, memory.size(1) // self.r, -1)
             T_decoder = memory.size(1)
 
-        initial_memory = Variable(
-            inputs.data.new(B, self.memory_dim * self.r).zero_())
+        initial_memory = Variable(tensor=inputs.data.new(B, self.memory_dim * self.r).zero_())
 
-        attention_rnn_hidden = Variable(inputs.data.new(B, 256).zero_())
-        decoder_rnn_hiddens = [Variable(inputs.data.new(B, 256).zero_())
-            for _ in range(len(self.decoder_rnns))]
-        current_context_vec = Variable(inputs.data.new(B, 256).zero_())
+        attention_rnn_hidden = Variable(tensor=inputs.data.new(B, 256).zero_())
+        decoder_rnn_hiddens = [Variable(tensor=inputs.data.new(B, 256).zero_()) for _ in range(len(self.decoder_rnns))]
+        current_context_vec = Variable(tensor=inputs.data.new(B, 256).zero_())
 
         if memory is not None:
             memory = memory.transpose(0, 1)
@@ -47,16 +47,15 @@ class Decoder(Module):
                 if greedy:
                     memory_input = outputs[-1]
                 else:
-                    memory_input = memory[t-1]
+                    memory_input = memory[t - 1]
 
             processed_memory = self.prenet(memory_input)
-            attention_rnn_hidden, current_context_vec, alignment = self.attention_rnn(
-                processed_memory, current_context_vec, attention_rnn_hidden, inputs)
-            decoder_input = self.project_to_decoder_in(
-                cat((attention_rnn_hidden, current_context_vec), -1))
+            attention_rnn_hidden, current_context_vec, alignment = self.attention_rnn(processed_memory,
+                                                                                      current_context_vec,
+                                                                                      attention_rnn_hidden, inputs)
+            decoder_input = self.project_to_decoder_in(cat(tensors=(attention_rnn_hidden, current_context_vec), dim=-1))
             for idx in range(len(self.decoder_rnns)):
-                decoder_rnn_hiddens[idx] = self.decoder_rnns[idx](
-                    decoder_input, decoder_rnn_hiddens[idx])
+                decoder_rnn_hiddens[idx] = self.decoder_rnns[idx](decoder_input, decoder_rnn_hiddens[idx])
                 # Residual connectinon
                 decoder_input = decoder_rnn_hiddens[idx] + decoder_input
             output = decoder_input
@@ -69,7 +68,8 @@ class Decoder(Module):
                 if t >= T_decoder:
                     break
             else:
-                if t > 1 and ((output.view(self.r, -1).data <= self.eps).prod(0) > 0).any() and alignment.data[:, int(alignment.shape[1]/2):].sum() > 0.7:
+                if t > 1 and ((output.view(self.r, -1).data <= self.eps).prod(0) > 0).any() and alignment.data[:, int(
+                        alignment.shape[1] / 2):].sum() > 0.7:
                     break
                 elif t > self.max_decoder_steps:
                     break
